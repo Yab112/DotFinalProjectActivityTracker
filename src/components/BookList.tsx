d55799a45f6fe6276d20e27735feb808f907ef11
yabibal <eshetieyabibal@gmail.com>
2024-12-12 17:16:59 -0500

    make the Booklist page responsive

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import BookCard from "./BookCard";
 import { fetchBooks } from "../../Services/BookService";
 import AddBookDialog from "./AddBookDialog";
-import { List, Book, BookDownIcon } from "lucide-react";
+import { List, BookDownIcon } from "lucide-react";
 
 interface Book {
   id: number;
@@ -33,23 +33,24 @@ const BookList: React.FC = () => {
 
   return (
     <section className="container mx-auto px-4 py-12">
-      <div className="flex justify-between items-center mb-8">  
-        <h2 className="text-2xl font-bold text-center text-cyan-700  flex justify-center items-center w-full">
+      <div className="flex flex-wrap justify-between items-center mb-8 gap-4">  
+        <h2 className="text-2xl font-bold text-center text-cyan-700 w-full sm:w-auto">
           Your Monochrome Library
         </h2>
-        <List className="h-8 w-8 inline-block mr-1 text-cyan-900 cursor-pointer" />
+        <List className="h-8 w-8 text-cyan-900 cursor-pointer" />
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {books.length > 0 ? (
           books.map((book, index) => <BookCard key={index} book={book} />)
         ) : (
-          <div className="flex flex-col items-center justify-center h-60 w-[40rem] bg-gray-100 rounded-lg shadow-md ml-[450px] p-2">
+          <div className="flex flex-col items-center justify-center h-60 w-full sm:w-96 bg-gray-100 rounded-lg shadow-md mx-auto p-4">
             <BookDownIcon className="h-24 w-24 text-cyan-400 mb-4" />
-            <div className="text-center text-gray-500 text-xl mb-4">
+            <div className="text-center text-gray-500 text-lg mb-4">
               No Books Here
             </div>
             <AddBookDialog />
-            <p className="text-center text-gray-800 mt-4 text-lg ml-4">
+            <p className="text-center text-gray-800 mt-4 text-sm sm:text-lg">
               Start adding books to build your library!
             </p>
           </div>
